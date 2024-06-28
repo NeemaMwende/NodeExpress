8970ac88be4a6ab67c4e5256c353b01b59f7b7da
NeemaMwende <gracepete12@gmail.com>
2024-06-29 00:41:03 +0300

    updates

--- form.js
+++ form.js
@@ -2,10 +2,21 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.statis('public'));
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "form.html"))
 });
 
-var server = app.listen(5000, function () {
+app.get('/process_get', function(req, res) {
+    response = {
+        first_name:req.query.first_name,
+        last_name:req.query.last_name,
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
+app.listen(5000, function () {
     console.log("Express App running on port 5000");
 });
